86fae7ff02249e218c253e8cf319d2111d7cf540
nerdcat822 <gusrbaos33@gmail.com>
2023-12-18 02:09:11 +0900

    streamlit

--- Home.py
+++ Home.py
@@ -1,11 +1,12 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
 
 st.title("Hello World")
 
 st.subheader("Welcome to Streamlit!")
 
-st.markdown(
-    """
-    #### I love it!
-"""
-)
+st.write([1, 2, 3, 4])
+st.write(PromptTemplate) # class 출력
+
+p = PromptTemplate.from_template("xxxx")
+st.write(p)
