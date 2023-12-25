e00b9ddf20c158a418853de1aae85be34510e403
nerdcat822 <gusrbaos33@gmail.com>
2023-12-25 23:43:58 +0900

    Complete QuizGPT

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,4 +1,4 @@
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
@@ -7,14 +7,8 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.memory import ConversationSummaryBufferMemory
-
 import streamlit as st
 
-"""
-    Challenge: Adding the Memory
-    It is worked!
-"""
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📃",
@@ -43,13 +37,6 @@ llm = ChatOpenAI(
     ],
 )
 
-# Create a memory
-memory = ConversationSummaryBufferMemory(
-    llm=llm,
-    max_token_limit=120,
-    memory_key="chat_history",
-    return_messages=True,
-)
 
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
@@ -75,9 +62,6 @@ def embed_file(file):
 def save_message(message, role):
     st.session_state["messages"].append({"message": message, "role": role})
 
-def save_memory(input, output):
-    st.session_state["chat_history"].append({"input": input, "output": output})
-
 
 def send_message(message, role, save=True):
     with st.chat_message(role):
@@ -94,21 +78,10 @@ def paint_history():
             save=False,
         )
 
-def restore_memory():
-    for history in st.session_state["chat_history"]:
-        memory.save_context({"input": history["input"]}, {"output": history["output"]})
 
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
-def load_memory(input):
-    return memory.load_memory_variables({})["chat_history"]
-
-def invoke_chain(message):
-    # invoke the chain
-    result = chain.invoke(message)
-    # save the interaction in the memory
-    save_memory(message, result.content)
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -120,8 +93,6 @@ prompt = ChatPromptTemplate.from_messages(
             Context: {context}
             """,
         ),
-        # passing chat history to the template? -> message placeholder
-        MessagesPlaceholder(variable_name="chat_history"),
         ("human", "{question}"),
     ]
 )
@@ -148,8 +119,6 @@ with st.sidebar:
 if file:
     retriever = embed_file(file)
     send_message("I'm ready! Ask away!", "ai", save=False)
-    # Restore memory and paint the history of previous chat
-    restore_memory()
     paint_history()
     message = st.chat_input("Ask anything about your file...")
     if message:
@@ -157,17 +126,14 @@ if file:
         chain = (
             {
                 "context": retriever | RunnableLambda(format_docs),
-                # sending the question straight to the prompt
-                "chat_history": load_memory,
                 "question": RunnablePassthrough(),
             }
             | prompt
             | llm
         )
         with st.chat_message("ai"):
-            invoke_chain(message)
+            chain.invoke(message)
 
 
 else:
     st.session_state["messages"] = []
-    st.session_state["chat_history"] = []
