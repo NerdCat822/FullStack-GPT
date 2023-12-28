0f246a007dd8720379fb0bbed41ab9f10552fec9
nerdcat822 <gusrbaos33@gmail.com>
2023-12-29 03:29:26 +0900

    make 3 function

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,4 +1,8 @@
 import subprocess
+from pydub import AudioSegment
+import math
+import openai
+import glob
 
 
 def extract_audio_from_video(video_path, audio_path):
@@ -12,7 +16,26 @@ def extract_audio_from_video(video_path, audio_path):
     subprocess.run(command)
 
 
-extract_audio_from_video(
-    "./files/podcast.mp4",
-    "./files/podcast.mp3",
-)
+def cut_audio_in_chunks(audio_path, chunk_size, chunks_folder):
+    track = AudioSegment.from_mp3(audio_path)
+    chunk_len = chunk_size * 60 * 1000
+    chunks = math.ceil(len(track) / chunk_len)
+    for i in range(chunks):
+        start_time = i * chunk_len
+        end_time = (i + 1) * chunk_len
+        chunk = track[start_time:end_time]
+        chunk.export(
+            f"./{chunks_folder}/chunk_{i}.mp3",
+            format="mp3",
+        )
+
+
+def transcribe_chunks(chunk_folder, destination):
+    files = glob.glob(f"{chunk_folder}/*.mp3")
+    for file in files:
+        with open(file, "rb") as audio_file, open(destination, "a") as text_file:
+            transcript = openai.Audio.transcribe(
+                "whisper-1",
+                audio_file,
+            )
+            text_file.write(transcript["text"])
